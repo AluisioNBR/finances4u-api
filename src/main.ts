b63121632817adbdb041ea100a1d3bd3c72c2cf4
AluisioNBR <lulu70638@gmail.com>
2023-03-10 18:56:33 -0300

    port fixed

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core'
+import { config } from 'dotenv'
 import { AppModule } from './modules/app.module'
+
+config()
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+	await app.listen(process.env.PORT ? process.env.PORT : 3000)
 }
 bootstrap()
